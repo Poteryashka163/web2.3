4fc9b65d0fb621c28242031564b1de0a043445fb
Admin <scrat0072@gmail.com>
2023-05-27 14:33:46 +0400

    Исправленный вариант.

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
     private final СalculationServise сalculationServise;
     public CalculatorController(СalculationServise сalculationServise){
@@ -12,30 +14,25 @@ public class CalculatorController {
     }
 
 
-    @GetMapping
+    @RequestMapping
     public String manu(){
-        return "В адресной строке введите: " +
-                " /calculator - для входа в калькулятор.";
+        return "Добро пожаловать в калькулятор.";
 
     }
-    @GetMapping(path="/calculator")
-    public String calculatorHello(){
-        return "Добро пожаловать в калькулятор.";
-    }
-    @GetMapping(path = "/calculator/plus")
-    public String add(@RequestParam(required = false) int num1, @RequestParam(required = false) int num2) {
+    @GetMapping(path = "/plus")
+    public String add(@RequestParam int num1, @RequestParam int num2) {
         return  сalculationServise.add(num1,num2);
     }
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String subtract(@RequestParam int num1,@RequestParam int num2) {
         return  сalculationServise.subtract(num1,num2);
     }
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiply(@RequestParam int num1,@RequestParam int num2) {
         return  сalculationServise.multiply(num1,num2);
 
     }
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String divide(@RequestParam int num1,@RequestParam int num2) {
         return сalculationServise.divide(num1,num2);
     }
